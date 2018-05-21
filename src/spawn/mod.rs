b45bc54158aa696f9228bdd5ebf841dab607dd26
Charles Bassett <chasb96@gmail.com>
2018-05-21 00:23:37 -0500

    Added Trait restrictions to Spawn Trait

--- src/spawn/mod.rs
+++ src/spawn/mod.rs
@@ -5,6 +5,9 @@ pub mod spawn;
 
 use std::error::Error;
 
-pub trait Spawns {
+/// Spawns needs Default + Clone for UX and safety reasons:
+///   Default to allow simple deriving for serde
+///   Clone to allow safe usage of a Spawn asynchronously
+pub trait Spawns: Default + Clone {
     fn spawn(&self) -> Result<(), Box<Error>>;
 }
